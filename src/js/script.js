d9245577278610a2458aa8e57f211dbf0ce7484f
Rostislav <spellmane21@gmail.com>
2023-09-13 04:01:10 +0300

    animations

--- src/js/script.js
+++ src/js/script.js
@@ -113,18 +113,142 @@ if (accordTitle) {
   accordCreate(accordTitle);
 }
 
+const isInView = (elem, bottom = 30) => {
+  // Get the top and bottom of the viewport.
+  const docViewTop = window.pageYOffset;
+  const docViewBottom = docViewTop + window.innerHeight;
+
+  // Get the top and bottom of the element.
+  const elemTop = elem.offsetTop;
+  const elemBottom = elemTop + bottom;
+
+  // Return true if the element is within the viewport.
+  return elemBottom <= docViewBottom && elemTop >= docViewTop;
+};
+
+const animations = {
+  fadeUp: [
+    {
+      isElementsCreated: false,
+      isElementsFired: false,
+    },
+    {
+      isElementsCreated: false,
+      isElementsFired: false,
+    },
+    {
+      isElementsCreated: false,
+      isElementsFired: false,
+    },
+    {
+      isElementsCreated: false,
+      isElementsFired: false,
+    },
+  ],
+};
+
 const initAnimations = () => {
-  const fadeUp = document.querySelectorAll(".fade-up");
-  fadeUp.forEach((element) => {
-    const container = element.dataset.container;
-    const child = document.createElement(container || "span");
-    child.classList.add("fade-up-child");
-    child.innerHTML = element.innerHTML;
-    element.innerHTML = "";
-    element.appendChild(child);
-  });
+  const initFadeUp = () => {
+    const fadeUp = document.querySelectorAll(".fade-up");
+    fadeUp.forEach((element, index) => {
+      if (!animations.fadeUp[index].isElementsCreated) {
+        const container = element.dataset.container;
+        const child = document.createElement(container || "span");
+        child.classList.add("fade-up-child");
+        child.innerHTML = element.innerHTML;
+        element.innerHTML = "";
+        element.appendChild(child);
+
+        animations.fadeUp[index].isElementsCreated = true;
+      }
+
+      if (isInView(element) && !animations.fadeUp[index].isElementsFired) {
+        element.querySelector(".fade-up-child").classList.add("visible");
+        animations.fadeUp[index].isElementsFired = true;
+      }
+    });
+  };
+
+  const initAbout = () => {
+    const about = document.querySelector("#about");
+    const isVisible = isInView(about, 300);
+
+    if (isVisible) {
+      const number = about.querySelector(".zoom-appear");
+      const slidesIn = about.querySelectorAll(".slideInRight");
+      const zoomDown = about.querySelector(".zoomDown");
+      number.classList.add("visible");
+      zoomDown.classList.add("visible");
+      slidesIn.forEach((slide) => slide.classList.add("visible"));
+    }
+  };
+
+  const initServices = () => {
+    const services = document.querySelector("#services");
+    const isVisible = isInView(services, 300);
+
+    if (isVisible) {
+      const number = services.querySelector(".zoom-appear");
+      const slidesIn = services.querySelectorAll(".slideInRight");
+      const slidesInLeft = services.querySelectorAll(".slideInLeftServices");
+      const zoomDown = services.querySelector(".zoomDown");
+
+      number.classList.add("visible");
+      zoomDown.classList.add("visible");
+      slidesIn.forEach((slide) => slide.classList.add("visible"));
+      slidesInLeft.forEach((slide) => slide.classList.add("visible"));
+    }
+  };
+
+  const initGlobal = () => {
+    const global = document.querySelector("#global");
+    const isVisible = isInView(global, 300);
+
+    if (isVisible) {
+      const numbers = global.querySelectorAll(".zoom-appear");
+      const slidesInRight = global.querySelectorAll(".slideInRight");
+      const slidesInLeft = global.querySelectorAll(".slideInLeft");
+      const zoomDown = global.querySelector(".zoomDown");
+      const slideTop = global.querySelector(".global__wrap-content");
+
+      slideTop.classList.add("visible");
+      zoomDown.classList.add("visible");
+      numbers.forEach((number) => number.classList.add("visible"));
+      slidesInRight.forEach((slide) => slide.classList.add("visible"));
+      slidesInLeft.forEach((slide) => slide.classList.add("visible"));
+    }
+  };
+
+  const initLocation = () => {
+    const locations = document.querySelector("#locations");
+    const isVisible = isInView(locations, 300);
+
+    if (isVisible) {
+      const slidesTop = locations.querySelector(".locations__wrap-content_bg");
+      const scale = locations.querySelector(".locations__wrap-map-template");
+      const slidesInRight = locations.querySelector(".slideInRight");
+      const slideInLeftLocation = locations.querySelectorAll(
+        ".slideInLeftLocation"
+      );
+
+      slidesTop.classList.add("visible");
+      scale.classList.add("visible");
+      slidesInRight.classList.add("visible");
+      slideInLeftLocation.forEach((number) => number.classList.add("visible"));
+    }
+  };
+
+  initFadeUp();
+  initAbout();
+  initServices();
+  initGlobal();
+  initLocation();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+  initAnimations();
+});
+
+document.addEventListener("scroll", () => {
   initAnimations();
 });
